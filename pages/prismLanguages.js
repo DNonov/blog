44858ae060e2d86a5e6598650486b536ccc68bea
Dimitar Nonov <d.nonov@gmail.com>
2021-08-17 21:19:22 +0100

    Add more languages to prism

--- pages/prismLanguages.js
+++ pages/prismLanguages.js
@@ -2,6 +2,7 @@ import "prismjs/components/prism-python";
 import "prismjs/components/prism-clojure";
 import "prismjs/components/prism-c";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-git";
 import "prismjs/components/prism-diff";
 import "prismjs/components/prism-vim";
@@ -10,6 +11,7 @@ import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-bash";
 import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-docker";
+import "prismjs/components/prism-haskell";
 
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
