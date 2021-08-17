3ad0f5f1458433566ad2765adabf14fcb90c39de
Dimitar Nonov <d.nonov@gmail.com>
2021-08-17 21:15:57 +0100

    Add Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import '../styles/prism.css'
+import '../styles/globals.css';
+import '../styles/prism.css';
+import '../styles/BlogPostTitleSection.css';
+import '../components/Layout';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
