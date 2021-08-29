960c2d1f185fd53fe2ac10ae6e7dad1efff33f3f
Dimitar Nonov <d.nonov@gmail.com>
2021-08-29 20:05:43 +0100

    Add extra space in metadata section
    Makes the metadata section more readable. There is nice separation
    between metadata label and content.

--- components/BlogPostTitleSection.jsx
+++ components/BlogPostTitleSection.jsx
@@ -3,8 +3,8 @@ export default function BlogPostTitleSection({data}) {
     <aside className="post-title-section">
       <h2 className="post-title">{data.title}</h2>
       <div className="post-metadata">
-        <span className="post-publish-date">Published at: {data.publishDate}</span>
-        <span className="post-publish-date">Tags: {data.tags}</span>
+        <span className="post-publish-date">Published at: &nbsp;{data.publishDate}</span>
+        <span className="post-publish-date">Tags: &nbsp;{data.tags}</span>
       </div>
     </aside>
   );
