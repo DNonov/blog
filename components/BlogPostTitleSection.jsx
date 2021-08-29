d31692a0eee26cab2318b4da666ab335490d1041
Dimitar Nonov <d.nonov@gmail.com>
2021-08-29 20:05:08 +0100

    Change blog post title form h1 to h2

--- components/BlogPostTitleSection.jsx
+++ components/BlogPostTitleSection.jsx
@@ -1,7 +1,7 @@
 export default function BlogPostTitleSection({data}) {
   return(
     <aside className="post-title-section">
-      <h1 className="post-title">{data.title}</h1>
+      <h2 className="post-title">{data.title}</h2>
       <div className="post-metadata">
         <span className="post-publish-date">Published at: {data.publishDate}</span>
         <span className="post-publish-date">Tags: {data.tags}</span>
