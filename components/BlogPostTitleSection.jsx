3bb1a303246286de049e95e76cfca73211c3ab57
Dimitar Nonov <d.nonov@gmail.com>
2021-08-29 19:37:50 +0100

    Display tags to blog post

--- components/BlogPostTitleSection.jsx
+++ components/BlogPostTitleSection.jsx
@@ -6,7 +6,7 @@ export default function BlogPostTitleSection({data}) {
       <h1 className="post-title">{data.title}</h1>
       <div className="post-metadata">
         <span className="post-publish-date">Published at: {data.publishDate}</span>
-        <Link href="/" className="all-posts-link">all posts</Link>
+        <span className="post-publish-date">Tags: {data.tags}</span>
       </div>
     </aside>
   );
