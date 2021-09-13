3bb7aeb19aed3d44632cac4599b695c5424b6e95
Dimitar Nonov <d.nonov@gmail.com>
2021-09-13 23:44:09 +0100

    Fix the bug related to math rendering

--- pages/[slug].js
+++ pages/[slug].js
@@ -10,19 +10,16 @@ import Head from "next/head";
 import BlogPostTitleSection from '../components/BlogPostTitleSection';
 
 const Post = ({htmlString, data}) => {
-  useEffect(() => prism.highlightAll(), [])
+  useEffect(() => {
+    prism.highlightAll();
+    renderMathInElement(document.body);
+  });
 
   return(
     <>
       <Head>
         <title>{data.title}</title>
         <meta title="description" content={data.description} />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/katex.min.css" integrity="sha384-6LkG2wmY8FK9E0vU9OOr8UvLwsaqUg9SETfpq4uTCN1agNe8HRdE9ABlk+fVx6gZ" crossOrigin="anonymous" />
-
-        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/katex.min.js" integrity="sha384-31El76TwmbHj4rF9DyLsygbq6xoIobG0W+jqXim+a3dU9W53tdH3A/ngRPxOzzaB" crossOrigin="anonymous"></script>
-
-        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/contrib/auto-render.min.js" integrity="sha384-vZTG03m+2yp6N6BNi5iM4rW4oIwk5DfcNdFfxkk9ZWpDriOkXX8voJBFrAO7MpVl" crossOrigin="anonymous"
-        onLoad="renderMathInElement(document.body);"></script>
       </Head>
       <div className="post-wrapper">
         <article className="post">
--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,22 @@ import "../styles/BlogPostTitleSection.css";
 import "../styles/Navbar.css";
 import "../components/Layout";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/katex.min.css" integrity="sha384-6LkG2wmY8FK9E0vU9OOr8UvLwsaqUg9SETfpq4uTCN1agNe8HRdE9ABlk+fVx6gZ" crossOrigin="anonymous" />
+
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/katex.min.js" integrity="sha384-31El76TwmbHj4rF9DyLsygbq6xoIobG0W+jqXim+a3dU9W53tdH3A/ngRPxOzzaB" crossOrigin="anonymous"></script>
+
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.16/dist/contrib/auto-render.min.js" integrity="sha384-vZTG03m+2yp6N6BNi5iM4rW4oIwk5DfcNdFfxkk9ZWpDriOkXX8voJBFrAO7MpVl" crossOrigin="anonymous" ></script>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
