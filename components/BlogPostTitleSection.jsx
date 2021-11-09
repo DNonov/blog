0ca612b4f5878482b9eb7457d743afce5dd2fef8
Dimitar Nonov <d.nonov@gmail.com>
2021-11-09 21:52:15 +0000

    Move the tag metadata to the new line on xs devices

--- components/BlogPostTitleSection.jsx
+++ components/BlogPostTitleSection.jsx
@@ -9,10 +9,10 @@ export default function BlogPostTitleSection({data}) {
       </Link>
       <div className="post-metadata">
         <span className="post-publish-date">
-          <b><i className="bi bi-calendar-date"></i>&nbsp;{data.publishDate} &nbsp; &nbsp;</b>
+          <b><i className="bi bi-calendar-date"></i>&nbsp;{data.publishDate} &nbsp;</b>
           <b><i className="bi bi-stopwatch"></i>&nbsp;{timeToFinishReading(data.content)} min read</b>
         </span>
-          <b><span className="post-publish-date"><i className="bi bi-tags"></i>&nbsp;{data.tags}</span></b>
+          <b><span className="post-tags"><i className="bi bi-tags"></i>&nbsp;{data.tags}</span></b>
       </div>
     </aside>
   );
