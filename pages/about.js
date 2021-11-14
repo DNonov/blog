ae5125698079949f96463c46a55534e7fdcb4f58
Dimitar Nonov <d.nonov@gmail.com>
2021-11-14 19:36:07 +0000

    Add about page and put some content for placeholder

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+  return (
+    <section className="content-wrapper">
+      <h1>Welcome to my brain dump!</h1>
+      <p>This is about!</p>
+    </section>
+  );
+};
+
+export default About;
