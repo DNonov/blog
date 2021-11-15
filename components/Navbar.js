2df495b1b2342cae30699f6024ccddbbd081b474
Dimitar Nonov <d.nonov@gmail.com>
2021-11-15 22:27:08 +0000

    Restyle the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,15 +3,17 @@ import Link from 'next/link';
 export default function Navbar() {
   return(
     <nav className="navbar">
-      <span className="logo">DNonov</span>
-      <ul className="navbar-menu">
-        <li className="navbar-menu-item">
-          <Link href={"/"}>Posts</Link>
-        </li>
-        <li className="navbar-menu-item">
-          <Link href={"/about"}>About</Link>
-        </li>
-      </ul>
+      <div className="navbar-inner-wrapper">
+        <span className="logo">DNonov</span>
+        <ul className="navbar-menu">
+          <li className="navbar-menu-item">
+            <Link href={"/"}>Posts</Link>
+          </li>
+          <li className="navbar-menu-item">
+            <Link href={"/about"}>About</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
