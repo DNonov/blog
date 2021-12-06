2a3a132a2d13e1f7dd39c46d1da8ce1feffea92f
Dimitar Nonov <d.nonov@gmail.com>
2021-12-06 23:32:59 +0000

    Add scroller

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Scroller from "./Scroller";
 
 export default function Layout({children}) {
   return(
     <>
       <Navbar />
       {children}
+      <Scroller />
       <Footer />
     </>
   );
